f5c9d69f89d8c6e261741665594ad468737cfcb1
MuhammadimYusoff <muhammadimyusoff@gmail.com>
2022-12-10 13:13:01 +0800

    first commit

---
+++ balanceTree/src/balanceTree/balanceTreeMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package balanceTree;
+
+/**
+ * @author MuYusoffJamaluddin
+ *
+ */
+public class balanceTreeMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
